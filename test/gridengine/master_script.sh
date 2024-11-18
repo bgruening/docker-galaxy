83134ab9272ab742ef4ed36872820d6f4177aa34
bgruening <bgruening@users.noreply.github.com>
2024-11-18 13:06:38 +0000

    deploy: bfdb2088e114d1574e3eb501f3194bd52d65936a

--- test/gridengine/master_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-useradd -u 1450 -m galaxy
-/usr/local/bin/setup_gridengine.sh
-tail -f /var/spool/gridengine/qmaster/messages
--- test/gridengine/setup_tool.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# cp tool_conf.xml config
-export GALAXY_CONFIG_TOOL_CONFIG_FILE=/galaxy/tool_conf.xml
-/usr/bin/startup
-tailf /home/galaxy/logs/*
